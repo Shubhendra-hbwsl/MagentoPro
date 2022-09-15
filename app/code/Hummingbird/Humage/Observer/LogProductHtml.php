a81dd0c4f64ac3cf3a32e47229d9d1cf7cd73582
Shubhendra Rajkarne <shubhendra.rajkarne@hbwsl.com>
2022-09-15 13:32:34 +0530

    USER STORY:3 LOG PRODUCT NAME AND ITS HTML IN SYSTEM.LOG
    use catalog_controller_product event to get the product name and
    use controller_front_send_response_before event to get the product html
    response.
    
    Limit the the html response to 20 lines.
    
    Psr\Log\LoggerInterface to log into the system.log file.

---
+++ app/code/Hummingbird/Humage/Observer/LogProductHtml.php
@@ -0,0 +1,20 @@
+<?php 
+
+namespace Hummingbird\Humage\Observer;
+
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Psr\Log\LoggerInterface;
+
+class LogProductHtml implements ObserverInterface{
+    private $_logger;   
+
+    public function __construct(LoggerInterface $logger){
+        $this->_logger = $logger;
+    }
+    
+    
+    public function execute(Observer $observer){
+        $this->_logger->info("PRODUCT LOG HTML: " . substr($observer->getData('response')->getBody(),0,20));
+    }
+}
